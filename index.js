1321b1e41e2287005939adc9f0c7b190483f80d2
Ioannis Gkikas <funkyfisch@gmail.com>
2020-04-09 20:12:01 +0200

    Move proxy generation logic to proxyGenerator.js
      * proxyGenerator.js is now responsible for all
        logic that generates all the proxies needed
        given correct configuration

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const serveStatic = require("serve-static")
 const history = require("connect-history-api-fallback")
 const { createProxyMiddleware } = require("http-proxy-middleware")
-const UI_PORT = process.env.UI_PORT
+
 const { getApiConfiguration } = require("./apiConfigurationParser")
+const { generateProxies } = require("./proxyGenerator.js")
+
+const UI_PORT = process.env.UI_PORT
 
 const apiConfig = getApiConfiguration()
 
@@ -15,33 +18,10 @@ const middleware = history({
 app.use(serveStatic(__dirname + "/dist"))
 app.use(middleware)
 
-/* Find way to configure http vs https */
-for (const endpoint of apiConfig.endpoints) {
-  const regexEndpointString = `^${endpoint.endpointString}`
+let proxies = generateProxies(apiConfig)
 
-  let trueHost
-  if (
-    !process.env[endpoint.host].startsWith("https://")
-    && !process.env[endpoint.host].startsWith("http://")
-  ) {
-    trueHost = `http://${process.env[endpoint.host]}`
-  } else {
-    trueHost = process.env[endpoint.host]
-  }
-
-  let proxy = {
-    target: `${trueHost}:${process.env[endpoint.port]}`,
-    changeOrigin: true,
-    pathRewrite: {
-      [regexEndpointString]: ""
-    }
-  }
-
-  if (endpoint.authentication === true) {
-    proxy.auth = `${process.env[endpoint.username]}:${process.env[endpoint.password]}`
-  }
-
-  app.use(createProxyMiddleware(`${endpoint.endpointString}`, proxy))
+for (const p of proxies) {
+  app.use(createProxyMiddleware(p.endpointString, p))
 }
 
 console.log(`App listening at port: ${UI_PORT}`)
