97003c6a34afb4e2dc9e71bb126ef3edc8f002ff
Ioannis Gkikas <ioannis.gkikas@aptiv.com>
2020-04-09 14:44:02 +0200

    Evaluate the environment variable name referenced in config
      * Instead of directly referencing the value in the
        configuration file, we use that value to refer to
        an environment variable, the value of which we
        then evaluate and use to create our proxy

--- index.js
+++ index.js
@@ -21,28 +21,22 @@ const middleware = history({
 app.use(serveStatic(__dirname + "/dist"))
 app.use(middleware)
 
-/* That's good and all, but we have to store Usernames and passwords in a
-json file. What we would really like is to map each api endpoint defined in the
-json file into environment variable strings and then evaluate them here,
-so that all this deployment dependent configuration can be defined in the
-environment instead of a file */
-
 /* Find way to configure http vs https */
 for (const endpoint of apiConfig.endpoints) {
   const regexEndpointString = `^${endpoint.endpointString}`
 
   let proxy = {
-    target: `${endpoint.host}:${endpoint.port}`,
+    target: `${process.env[endpoint.host]}:${process.env[endpoint.port]}`,
     changeOrigin: true,
     pathRewrite: {
       [regexEndpointString]: ""
     }
   }
   if (endpoint.authentication === true) {
-    proxy.auth = `${endpoint.username}:${endpoint.password}`
+    proxy.auth = `${process.env[endpoint.username]}:${process.env[endpoint.password]}`
   }
 
-  app.use([endpoint.endpointString], createProxyMiddleware(proxy))
+  app.use(createProxyMiddleware(`${endpoint.endpointString}`, proxy))
 }
 
 console.log(`App listening at port: ${UI_PORT}`)
