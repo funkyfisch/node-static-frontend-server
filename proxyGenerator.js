c4a1426e72d7004b27f4e1303f9910305dd8b8c4
Ioannis Gkikas <ioannis.gkikas@aptiv.com>
2020-04-09 20:12:01 +0200

    Move proxy generation logic to proxyGenerator.js
      * proxyGenerator.js is now responsible for all
        logic that generates all the proxies needed
        given correct configuration

---
+++ proxyGenerator.js
@@ -0,0 +1,37 @@
+/* Find way to configure http vs https */
+const generateProxies = apiConfig => {
+  let proxies = []
+  for (const endpoint of apiConfig.endpoints) {
+    const regexEndpointString = `^${endpoint.endpointString}`
+
+    let trueHost
+    if (
+      !process.env[endpoint.host].startsWith("https://")
+        && !process.env[endpoint.host].startsWith("http://")
+    ) {
+      trueHost = `http://${process.env[endpoint.host]}`
+    } else {
+      trueHost = process.env[endpoint.host]
+    }
+
+    let proxy = {
+      target: `${trueHost}:${process.env[endpoint.port]}`,
+      changeOrigin: true,
+      pathRewrite: {
+        [regexEndpointString]: ""
+      }
+    }
+
+    if (endpoint.authentication === true) {
+      proxy.auth = `${process.env[endpoint.username]}:${process.env[endpoint.password]}`
+    }
+
+    proxy.endpointString = endpoint.endpointString
+    proxies.push(proxy)
+  }
+  return proxies
+}
+
+module.exports = {
+  generateProxies
+}
