92075a4fbf4e95f230507a1fcac7e5fb8709c757
Ioannis Gkikas <ioannis.gkikas@aptiv.com>
2020-04-09 13:43:08 +0200

    Generate proxy table based on configuration from json file
      * The different proxies can be now defined based on JSON
        data imported from apiConfiguration.json

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
+const fs = require("fs")
 const express = require("express")
 const serveStatic = require("serve-static")
 const history = require("connect-history-api-fallback")
 const { createProxyMiddleware } = require("http-proxy-middleware")
 const UI_PORT = process.env.UI_PORT
 
-/* The api host is what the front-end client will access during its lifecycle */
-const API_HOST = process.env.API_HOST
-const API_PORT = process.env.API_PORT
-const API_USER = process.env.API_USER
-const API_PASSWORD = process.env.API_PASSWORD
-
-/* TODO: make the definition of the API endpoint(s) customizable using a .json
-file, or another manner, and then auto-generate the endpoints and add them
-to the proxy-table */
+let apiConfig
+try {
+  apiConfig = fs.readFileSync("apiConfiguration.json")
+  apiConfig = JSON.parse(apiConfig)
+} catch (error) {
+  throw new Error("Please create a valid configuration file for your APIs")
+}
 
 const app = express()
 
 const middleware = history({
-    verbose: true
+  verbose: true
 })
 app.use(serveStatic(__dirname + "/dist"))
 app.use(middleware)
-app.use("/api", createProxyMiddleware({
-    target: `http://${API_HOST}:${API_PORT}`,
-    auth: `${API_USER}:${API_PASSWORD}`,
+
+/* That's good and all, but we have to store Usernames and passwords in a
+json file. What we would really like is to map each api endpoint defined in the
+json file into environment variable strings and then evaluate them here,
+so that all this deployment dependent configuration can be defined in the
+environment instead of a file */
+for (const endpoint of apiConfig.endpoints) {
+  const regexEndpointString = `^${endpoint.endpointString}`
+
+  let proxy = {
+    target: `${endpoint.host}:${endpoint.port}`,
     changeOrigin: true,
     pathRewrite: {
-        "^/api" : ""
+      [regexEndpointString]: ""
     }
-}))
+  }
+  if (endpoint.authentication === true) {
+    proxy.auth = `${endpoint.user}:${endpoint.password}`
+  }
+
+  app.use([endpoint.endpointString], createProxyMiddleware(proxy))
+}
 
 console.log(`App listening at port: ${UI_PORT}`)
 app.listen(UI_PORT)
